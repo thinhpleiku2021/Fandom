965077a879dcdcaef5f64fcecc2e2c58a66b158f
Nguyễn Gia Thịnh <93753773+thinhpleiku2021@users.noreply.github.com>
2022-04-18 10:54:30 +0700

    vsdfsdgsd

---
+++ javascript.js
@@ -0,0 +1,33 @@
+// var x = [3,5,2,9,1,8,7];
+// var max = x[0];
+// for(var i = 0;i<x.length;i++){
+//     if(x[i]>max){
+//         max = x[i];
+//     }
+// }
+// console.log(max);
+
+// var x = [3,5,2,9,1,8,7];
+// var min = x[0];
+// for(var i = 0;i<x.length;i++){
+//     if(x[i]<min){
+//         min = x[i];
+//     }
+// }
+// console.log(min);
+
+// var x = [1,2,3,4,5,6];
+// for(var i = 0;i < x.length;i++){
+//     if(x[i] % 2 == 0){
+//         console.log(x[i]);
+//     }
+// }
+
+// var x = [1,2,3,4,5,6];
+// for(var i = 0;i < x.length;i++){
+//     if(x[i] %2 !=0){
+//         console.log(x[i]);
+//     }
+// }
+
+
